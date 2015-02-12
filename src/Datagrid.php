971af8890d76fa4e554d0d090d7f9695f24c2415
Tomas Kudelka <tomas@appsdevteam.com>
2015-02-12 13:10:25 +0100

    fix separateFilter

--- src/Datagrid.php
+++ src/Datagrid.php
@@ -39,6 +39,14 @@ class Datagrid extends UI\Control
 	/** @persistent */
 	public $page = 1;
 
+	/**
+	 * Má se filtr vykreslovat jako oddělený formulář? Pokud ano, tak pro
+	 * vykreslování filtru používáme {control myGridControl:filter} a pro grid
+	 * {control myGridControl}. Pokud ne, tak pro celý grid {control myGridControl}.
+	 * @var boolean
+	 */
+	public $separateFilter = FALSE;
+
 	/** @var array */
 	protected $filterDataSource = array();
 
@@ -259,6 +267,17 @@ class Datagrid extends UI\Control
 
 	public function render()
 	{
+		$this->template->separateFilter = $this->separateFilter;
+		$this->template->render = NULL;
+		$this->renderDefault();
+	}
+
+	public function renderFilter() {
+		$this->template->render = 'filter';
+		$this->renderDefault();
+	}
+
+	protected function renderDefault() {
 		if ($this->filterFormFactory) {
 			$this['form']['filter']->setDefaults($this->filter);
 		}
@@ -517,6 +536,9 @@ class Datagrid extends UI\Control
 				}
 				$this->filter = $this->filterDataSource = $values;
 				$this->redrawControl('rows');
+				if ($this->separateFilter) {
+					$this->redrawControl('filter');
+				}
 			} elseif ($form['filter']['cancel']->isSubmittedBy()) {
 				if ($this->paginator) {
 					$this->page = $this->paginator->page = 1;
@@ -524,6 +546,9 @@ class Datagrid extends UI\Control
 				$this->filter = $this->filterDataSource = $this->filterDefaults;
 				$form['filter']->setValues($this->filter, TRUE);
 				$this->redrawControl('rows');
+				if ($this->separateFilter) {
+					$this->redrawControl('filter');
+				}
 			}
 		}
 
